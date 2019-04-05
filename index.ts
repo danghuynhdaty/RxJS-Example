29ed1dab0b1a1928cf83ec04665723b37895a0ef
danghuynhdaty <daty.danghuynh20497@gmail.com>
2019-04-05 23:32:35 +0700

    Add map operator example

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import { of } from 'rxjs'; 
 import { map } from 'rxjs/operators';
-
+import data from './map';
 
 const source = of('World').pipe(
-  map(x => `Hello ${x}!`)
+  map(x => `Hello ${x}! \n Run the example by copy and paste content into index`)
 );
 
 source.subscribe(x => console.log(x));
